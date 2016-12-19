f2ef5362df27fc685f6013254a2b5309651c4002
Marc Laval <mlaval@amadeus.com>
2016-12-19 18:31:31 +0100

    1.2.0

---
+++ src/webpack.js
@@ -0,0 +1,3 @@
+// Declarations for (some) Webpack types. Only what's needed.
+"use strict";
+//# sourceMappingURL=/Users/mlaval/dev/github/angular-cli_mlaval/packages/@ngtools/webpack/src/webpack.js.map
